c794b208a95b36b54c1760b19e0622071ec04bdf
BigBoa19 <choinoah2005@gmail.com>
2025-03-24 18:26:05 -0400

    new file:   noahchoi-lab6/index.html 	new file:   noahchoi-lab6/static/Noah Choi Resume.pdf 	new file:   noahchoi-lab6/static/clown.jpg 	new file:   noahchoi-lab6/static/kanye.jpg 	new file:   noahchoi-lab6/static/script.js 	new file:   noahchoi-lab6/static/style.css 	new file:   noahchoi-lab6/static/style2.css 	new file:   noahchoi-lab6/template.html 	new file:   noahchoi-lab6/views/contact.html 	new file:   noahchoi-lab6/views/cv.html 	new file:   noahchoi-lab6/views/fun.html

---
+++ noahchoi-lab6/static/script.js
@@ -0,0 +1,90 @@
+function greetingFunc(){
+    let d = new Date();
+    let h = d.getHours();
+    let msg = "";
+    if(h < 5){
+        msg = "Good Night"
+        console.log("Good Night");
+    }
+    else if(h < 12){
+        msg = "Good Morning";
+        console.log("Good Morning");
+    } else if(h < 18){
+        msg = "Good Afternoon";
+        console.log("Good Afternoon");
+    } else if(h < 20){
+        msg = "Good Evening";
+        console.log("Good Evening");
+    } else{
+        msg = "Good Night";
+        console.log("Good Night");
+    }
+
+    if(window.location.pathname.endsWith("index.html")){
+        let E = document.getElementById("Greeting")
+        if(E) E.innerHTML = msg + ", my name is Noah.";
+    }
+}
+
+greetingFunc();
+
+function addYear(){
+    let d = new Date();
+    let y = d.getFullYear();
+    document.getElementById("copyYear").innerHTML = y + " designed and coded by Noah Choi";
+}
+
+// function showList(){
+//     document.getElementById("see-more").style.display = "none";
+//     document.getElementById("FavList").style.display = "block";
+// }
+
+
+if(window.location.pathname.endsWith("index.html")){
+    $(document).ready(function(){
+        $("#read-more").click(function(){
+            $("#short-intro").hide();
+            $("#read-more").hide();
+            $("#long-intro").show();
+            $("#read-less").show();
+        });
+        $("#read-less").click(function(){
+            $("#long-intro").hide();
+            $("#read-less").hide();
+            $("#short-intro").show();
+            $("#read-more").show();
+        });
+    });
+}
+
+
+function validate() {
+    let name = document.getElementById("fname");
+    let email = document.getElementById("femail");
+    let message = document.getElementById("fmessage");
+    let validity = document.getElementById("validity-msg");
+    if(!name.checkValidity() || !email.checkValidity() || !message.checkValidity()){
+        validity.innerHTML = "Please fill out all required fields"
+    }
+}
+
+function getAdvice() {
+    fetch("https://api.adviceslip.com/advice")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            const advice = data.slip.advice;
+            document.getElementById("adviceText").innerText = advice;
+        })
+        .catch(error => console.error(error));
+}
+
+links = Array.from(document.querySelectorAll('a'));
+
+links.forEach(element => {
+    console.log(element.href);
+    console.log(window.location.pathname);
+    if(element.href.includes(window.location.pathname)){
+        element.classList.add('active');
+    }
+});
